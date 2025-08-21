4c44b0cb87322532e06957443594b54e5a4cd9c9
Kirby144p <86081961+Kirby144p@users.noreply.github.com>
2025-08-21 22:38:03 +0200

    Added missing license text.

--- src/client/java/kirby144p/ssmc/mixin/client/ChatOptionsScreenMixin.java
+++ src/client/java/kirby144p/ssmc/mixin/client/ChatOptionsScreenMixin.java
@@ -1,3 +1,20 @@
+//	Stop spamming my Chat (SSMC) is a modification for Minecraft which
+//  allows for filtering and performing certain actions on chat messages.
+//	Copyright (C) 2025  Kirby144p
+//	
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//	
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//	
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package kirby144p.ssmc.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
